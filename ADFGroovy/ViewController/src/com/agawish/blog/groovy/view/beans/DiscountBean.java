afe0c9e53cf5bd24c3b8fdecf57d54ea5bc51bfd
Amr Gawish <amr.gawish@gmail.com>
2015-06-11 15:22:01 +0100

    Adding ADF Groovy Project
    How to execute Groovy script from ADF

---
+++ ADFGroovy/ViewController/src/com/agawish/blog/groovy/view/beans/DiscountBean.java
@@ -0,0 +1,60 @@
+package com.agawish.blog.groovy.view.beans;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DiscountBean {
+    private Double subtotal;
+    private Double total;
+
+    public DiscountBean() {
+        super();
+    }
+
+    public String submitSubtotal() throws IOException {
+        //Path to physical file
+        //TODO Change me
+        File discountFile =
+            new File("C:\\Users\\amr\\dev\\ADFGroovy\\ViewController\\public_html\\WEB-INF\\discountvalue.groovy");
+        String discountValue = "";
+        if (discountFile.exists()) {
+            Binding binding = new Binding();
+
+            GroovyShell shell =
+                new GroovyShell(this.getClass().getClassLoader(), binding);
+            Object value;
+            value = shell.evaluate(discountFile);
+            if (value != null) {
+                if (value instanceof String) {
+                    discountValue = (String)value;
+                }
+            }
+            //Calculate the amount of discount to subtract
+            Double discount =
+                (Double.parseDouble(discountValue) / 100) * subtotal;
+            
+            //Get the total
+            total = subtotal - discount;
+        }
+        return null;
+    }
+
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+}
